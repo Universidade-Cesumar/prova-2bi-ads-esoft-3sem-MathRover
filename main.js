48dccdbce19c8e2c73612f2ac9f3cb9ad62288d4
Matheus Rover <rovermatheus@gmail.com>
2026-06-10 20:15:56 -0300

    Refatorando JS, fazendo a função para buscar e exibir materiais

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-const novoMaterial = {
-  nome: document.getElementById('input-nome').value,
-  quantidade: parseInt(document.getElementById('input-quantidade').value)
-};
+function carregarMateriais() {
+    fetch('https://6a29e35ff59cb8f65f1db45f.mockapi.io/itens')
+        .then(res => res.json())
+        .then(dados => {
+            const tbody = document.querySelector('#lista-materiais tbody');
+            tbody.innerHTML = '';
+            dados.forEach(item => {
+                tbody.innerHTML += `
+                    <tr>
+                        <td>${item.id}</td>
+                        <td>${item.nome}</td>
+                        <td>${item.quantidade}</td>
+                    </tr>
+                `;
+            });
+        })
+        .catch(error => console.error('Erro ao carregar:', error));
+}
 
-fetch('https://6a29e35ff59cb8f65f1db45f.mockapi.io/itens', {
-  method: 'POST',
-  headers: {'content-type':'application/json'},
-  body: JSON.stringify(novoMaterial)
-})
-.then(res => {
-  if (res.ok) {
-    return res.json();
-  }
-  throw new Error('Erro na requisição');
-})
-.then(material => {
-  console.log('Material cadastrado:', material);
-  document.getElementById('input-nome').value = '';
-  document.getElementById('input-quantidade').value = '';
-  carregarMateriais();
-})
-.catch(error => {
-  console.error('Erro:', error);
-  alert('Erro ao cadastrar material');
-});
+carregarMateriais();
