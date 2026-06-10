04bdc30cf20bb677e1d545b7f9f90a58cb5ce686
Matheus Rover <rovermatheus@gmail.com>
2026-06-10 19:51:47 -0300

    Iniciando JS da página, usando código da documentação para iniciar

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Arquivo para código javascript
+const newTask = {
+  content: 'Check out mockapi.io',
+  completed: false,
+};
+
+fetch('https://PROJECT_TOKEN.mockapi.io/tasks', {
+  method: 'POST',
+  headers: {'content-type':'application/json'},
+  // Send your data in the request body as JSON
+  body: JSON.stringify(newTask)
+}).then(res => {
+  if (res.ok) {
+      return res.json();
+  }
+  // handle error
+}).then(task => {
+  // do something with the new task
+}).catch(error => {
+  // handle error
+})
