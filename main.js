57ff4b9b5ed3b417c58209bd6602ed6338f6056d
Matheus Rover <rovermatheus@gmail.com>
2026-06-10 20:23:39 -0300

    Arrumando erro de duplicação de produtos

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// PARTE 1: Função para carregar materiais
 function carregarMateriais() {
     fetch('https://6a29e35ff59cb8f65f1db45f.mockapi.io/itens')
         .then(res => res.json())
@@ -10,14 +11,12 @@ function carregarMateriais() {
                         <td>${item.id}</td>
                         <td>${item.nome}</td>
                         <td>${item.quantidade}</td>
-                    </tr>
+                    </table>
                 `;
             });
         })
         .catch(error => console.error('Erro ao carregar:', error));
 }
-
-carregarMateriais();
 
 document.getElementById('btn-cadastrar').addEventListener('click', function() {
     console.log('Botão foi clicado!');
@@ -27,40 +26,6 @@ document.getElementById('btn-cadastrar').addEventListener('click', function() {
     
     console.log('Nome:', nome);
     console.log('Quantidade:', quantidade);
-});
-
-document.getElementById('btn-cadastrar').addEventListener('click', function() {
-    const nome = document.getElementById('input-nome').value;
-    const quantidade = parseInt(document.getElementById('input-quantidade').value);
-    
-    const novoMaterial = {
-        nome: nome,
-        quantidade: quantidade
-    };
-
-    fetch('https://6a29e35ff59cb8f65f1db45f.mockapi.io/itens', {
-        method: 'POST',
-        headers: {'content-type':'application/json'},
-        body: JSON.stringify(novoMaterial)
-    })
-    .then(res => {
-        if (res.ok) {
-            return res.json();
-        }
-        throw new Error('Erro na requisição');
-    })
-    .then(material => {
-        console.log('Material cadastrado:', material);
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-        alert('Erro ao cadastrar material');
-    });
-});
-
-document.getElementById('btn-cadastrar').addEventListener('click', function() {
-    const nome = document.getElementById('input-nome').value;
-    const quantidade = parseInt(document.getElementById('input-quantidade').value);
     
     const novoMaterial = {
         nome: nome,
@@ -81,11 +46,9 @@ document.getElementById('btn-cadastrar').addEventListener('click', function() {
     .then(material => {
         console.log('Material cadastrado:', material);
         
-        // Limpar os inputs
         document.getElementById('input-nome').value = '';
         document.getElementById('input-quantidade').value = '';
         
-        // Recarregar a tabela
         carregarMateriais();
     })
     .catch(error => {
@@ -93,3 +56,5 @@ document.getElementById('btn-cadastrar').addEventListener('click', function() {
         alert('Erro ao cadastrar material');
     });
 });
+
+carregarMateriais();
