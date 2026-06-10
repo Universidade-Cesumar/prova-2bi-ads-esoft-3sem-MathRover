8d17e52d7accb85423003d2b93173686c8a9f689
Matheus Rover <rovermatheus@gmail.com>
2026-06-10 20:20:36 -0300

    Adicionando função para buscar e exibir materiais na tabela

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ document.getElementById('btn-cadastrar').addEventListener('click', function() {
     console.log('Nome:', nome);
     console.log('Quantidade:', quantidade);
 });
+
+document.getElementById('btn-cadastrar').addEventListener('click', function() {
+    const nome = document.getElementById('input-nome').value;
+    const quantidade = parseInt(document.getElementById('input-quantidade').value);
+    
+    const novoMaterial = {
+        nome: nome,
+        quantidade: quantidade
+    };
+
+    fetch('https://6a29e35ff59cb8f65f1db45f.mockapi.io/itens', {
+        method: 'POST',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(novoMaterial)
+    })
+    .then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error('Erro na requisição');
+    })
+    .then(material => {
+        console.log('Material cadastrado:', material);
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao cadastrar material');
+    });
+});
